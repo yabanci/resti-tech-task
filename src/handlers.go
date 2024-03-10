0c8f59b853461ba94d1f3b1f276437cfd7384ef3
Arsen <ozhetov.arsen@gmail.com>
2024-03-11 03:41:16 +0500

    feat: validation of accounts and balance

--- src/handlers.go
+++ src/handlers.go
@@ -44,14 +44,51 @@ func getAccounts(c *gin.Context) {
   c.JSON(http.StatusOK, accounts)
 }
 
+func validateAccountID(accountID int) bool {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM accounts WHERE id = $1", accountID).Scan(&count)
+	if err != nil {
+			log.Printf("Error querying account ID: %v\n", err)
+			return false
+	}
+	return count == 1
+}
+
+func getAccountBalance(accountID int) (float64, error) {
+	var balance float64
+	err := db.QueryRow("SELECT balance FROM accounts WHERE id = $1", accountID).Scan(&balance)
+	if err != nil {
+			return 0, err
+	}
+	return balance, nil
+}
+
 func createTransaction(c *gin.Context) {
-	// todo: check account_ids (is valid, do we have one)
-	// todo: check balance of first account_id
+	// todo: after creating a transaction, change balance of accounts
   var transaction Transaction
   if err := c.ShouldBindJSON(&transaction); err != nil {
     c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
     return
   }
+
+	if !validateAccountID(transaction.AccountID) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid sender account ID"})
+		return
+	}
+	if !validateAccountID(transaction.Account2ID) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid receiver account ID"})
+		return
+	}
+
+	senderBalance, err := getAccountBalance(transaction.AccountID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching sender account balance"})
+		return
+	}
+	if senderBalance < transaction.Value {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient balance"})
+		return
+	}
 
   _, err := db.Exec("INSERT INTO transactions (value, account_id, group_type, account2_id, date) VALUES ($1, $2, $3, $4, $5)", transaction.Value, transaction.AccountID, transaction.Group, transaction.Account2ID, transaction.Date)
 	if err != nil {
