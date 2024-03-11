72d30093617fb25d86aeca17bdad508853297c55
Arsen <ozhetov.arsen@gmail.com>
2024-03-11 05:43:36 +0500

    refactor: main.go

--- main.go
+++ main.go
@@ -5,36 +5,46 @@ import (
   "fmt"
   "log"
   "net/http"
+  "os"
+
   "github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+  "github.com/joho/godotenv"
   _ "github.com/lib/pq"
+  "repositories"
+  "controllers"
 )
 
 var db *sql.DB
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+  if err := godotenv.Load(); err != nil {
+    log.Fatalf("Error loading .env file: %v", err)
+  }
 
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
+  dbUser := os.Getenv("DB_USER")
+  dbPassword := os.Getenv("DB_PASSWORD")
+  dbName := os.Getenv("DB_NAME")
 
-	dbURL := fmt.Sprintf("postgres://%s:%s@db:5432/%s?sslmode=disable", dbUser, dbPassword, dbName)
+  dbURL := fmt.Sprintf("postgres://%s:%s@db:5432/%s?sslmode=disable", dbUser, dbPassword, dbName)
 
-	db, err := sql.Open("postgres", dbURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+  db, err := sql.Open("postgres", dbURL)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+  accountRepo := repositories.NewAccountRepository(db)
+  accountController := controllers.NewAccountController(accountRepo)
+
+  transactionRepo := repositories.NewTransactionRepository(db)
+  transactionController := controllers.NewTransactionController(transactionRepo)
 
   router := gin.Default()
 
-  router.POST("/accounts", createAccount)
-  router.GET("/accounts", getAccounts)
-  router.POST("/transactions", createTransaction)
-  router.GET("/transactions/:account_id", getTransactions)
+  router.POST("/accounts", accountController.CreateAccount)
+  router.GET("/accounts", accountController.GetAccounts)
+  router.POST("/transactions", transactionController.CreateTransaction)
+  router.GET("/transactions/:account_id", transactionController.GetTransactions)
 
   router.Run(":8080")
 }
